e517a5e478fffb0232242005bc6361f37ce2801e
hsabahy <hsabahy@gmail.com>
2025-06-23 14:45:41 +0300

    Primitives Data

---
+++ PrimitivesData.js
@@ -0,0 +1,19 @@
+//Primitives data types
+//Reuseable variables
+let name = 'Heba';
+let age = 30;
+let isBlackBelt = false;
+
+console.log(name, age, isBlackBelt);
+
+//Non reuseable variables
+const lastName = 'Ahmed';
+
+console.log(lastName);
+
+//Camel Case
+// let firstName = 'Heba';
+//Pascal Case
+// let FirstName = 'Heba';
+//Snake Case
+// let first_name = 'Heba';
